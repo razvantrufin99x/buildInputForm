e8a51656d86b0e896a151f9701ceff4fec102509
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-03-03 16:53:27 +0200

    noi optiun iadaugate precum grup, datafield,list si listade lista si evenimente

---
+++ buildInputForm/listofdatafieldsgroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buildInputForm
+{
+    public class listofdatafieldsgroup
+
+    {
+        public List<groupofdatafields> datafieldsgrouplist = new List<groupofdatafields>();
+        public void addDataFieldinList(groupofdatafields d)
+        {
+            datafieldsgrouplist.Add(d);
+        }
+        public listofdatafieldsgroup() { }
+
+    }
+}
---
+++ buildInputForm/listwithalldatafieldslists.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buildInputForm
+{
+    public class listwithalldatafieldslists
+    {
+        public List<listwithalldatafieldslists> listaswithallthedatafieldslists = new List<listwithalldatafieldslists>();
+        public listwithalldatafieldslists() { }
+        public void addNewlistaswithallthedatafieldslists(listwithalldatafieldslists l)
+        {
+            listaswithallthedatafieldslists.Add(l);
+        }
+    }
+}
